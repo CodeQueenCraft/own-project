6b931505e8800ca90d7fd17e51bf86568ec5baa6
Koschel90 <betina.koschel@stud.h-da.de>
2023-10-24 12:53:47 +0200

    first deply

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing
+//JSX: JavaScript XML
+function Message (){
+    const name = 'Beti';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
